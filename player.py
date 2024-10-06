67c4a577361add6d29558e36cd74cc88e98cb271
Daxin319 <lylehigg@gmail.com>
2024-10-06 14:40:16 -0600

    added asteroid.py and asteroidfield.py

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from circleshape import *
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x, y, PLAYER_RADIUS)
+        super().__init__(x, y, radius)
         self.rotation = 0
 
     def triangle(self):
